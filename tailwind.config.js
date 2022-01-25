97b41e5904939a002fad5fc48428fee1dd8d72e3
Rémi Martineau <remi.martineau.dev@gmail.com>
2022-01-25 12:18:41 +0100

    project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: "hsl(176, 50%, 47%)",
+        darkCyan: "hsl(176, 72%, 28%)",
+        darkGray: "hsl(0, 0%, 48%)",
+        black: "hsl(0, 0%, 0%)",
+
+      }
+    },
   },
   plugins: [],
 }
